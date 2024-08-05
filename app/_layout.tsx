a8364598c285375bf8926863194b06d11e78b8a2
jdhernandez1832 <jdhernandez1832@soy.sena.edu.co>
2024-08-04 23:40:48 -0500

    segundo video

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function RootLayout() {
+const RootLayout = ()=>{
   return (
     <Stack>
-      <Stack.Screen name="index" />
+        <Stack.Screen name="index" options={{ headerShown: false }}/>
     </Stack>
-  );
+  )
 }
+
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+
+const RootLayout = () => {
+  return <Slot/>
 }
+
+export default RootLayout
