b70fbfe7f122e12578ecad9bd57696105467c721
jdhernandez1832 <jdhernandez1832@soy.sena.edu.co>
2024-08-05 01:00:26 -0500

    Se agrego tailwind, se agrego profile.tsx, se borraron carpetas de images y fonts, cambiando de nombre de copy a normal

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
-import { Slot } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
 
-
-const RootLayout = () => {
-  return <Slot/>
+export default function App(){
+  return(
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl"> Aora ADSO!</Text>
+      <StatusBar style='auto'/>
+      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
+    </View>
+  )
 }
-
-export default RootLayout
