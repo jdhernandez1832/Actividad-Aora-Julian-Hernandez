b65c0bc09fba4a6323bd234e793f0deae0f4de66
jdhernandez1832 <jdhernandez1832@soy.sena.edu.co>
2024-08-20 00:46:41 -0500

    se agrego carpeta nueva de auth y tabs con sus respectivos archivos asi como se modifico el layout de tabs

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ export default function App(){
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl"> Aora ADSO!</Text>
       <StatusBar style='auto'/>
-      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
+      <Link href="/home" style={{color: 'blue'}}>Go to home</Link>
     </View>
   )
 }
